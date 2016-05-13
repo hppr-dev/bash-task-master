d072581b36421ca1c58756c0a47f49bcfee80f13
Stephen Walker <swalker@redhat.com>
2016-05-13 11:50:56 -0400

    update gen lib functions

--- dogfood/tasks.sh
+++ dogfood/tasks.sh
@@ -1,18 +1,15 @@
+LOCAL_TASKS_UUID=dogfood
 task_edit() {
   vim /home/swalker/.task-master/dogfood/tasks.sh
 }
 
 # Task to update the lib-functions.sh with the current global functions
 task_gen-lib-func() {
-    pushd `pwd` > /dev/null
-    cd /home/swalker/.task-master/dogfood
-    cd ..
+    cd $TASK_MASTER_HOME
     grep -h -I -R lib -e ".*()" | sed 's/\(.*\)().*/readonly -f \1/' | grep -v '\$' > lib-functions.sh
     echo >> lib-functions.sh
     grep -h -I global.sh -e ".*()" | sed 's/\(.*\)().*/readonly -f \1/' | grep -v '\$' | grep -v 'task_edit' >> lib-functions.sh
-    popd > /dev/null
 }
-
 task_tester() {
   if [[ $TASK_SUBCOMMAND == "set" ]]
   then
@@ -22,5 +19,32 @@ task_tester() {
   then
     release_var "PS1"
   fi
+
+  #Recorded subcommand
+  if [[ $TASK_SUBCOMMAND == "poop" ]]
+  then
+    task global debug -c record
+  fi
 }
 
+task_env() {
+  #Recorded subcommand
+  if [[ $TASK_SUBCOMMAND == "diff" ]]
+  then
+    cd /home/swalker/.task-master/dogfood
+    env > env.before
+    set > set.before
+    $ARG_PROC
+    env > env.after
+    set > set.after
+    echo "Environment Additions"
+    diff env.before env.after | grep -e '>' 
+    echo "Set Additions"
+    diff set.before set.after | grep -e '>'
+    rm env.{before,after} set.{before,after}
+  fi
+}
+arguments_env() {
+  DIFF_REQUIREMENTS="proc:p:str"
+  SUBCOMMANDS="diff"
+}
