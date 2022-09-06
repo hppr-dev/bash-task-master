f1d0d911109d611bc891a2cd1f602e96d8774e1b
Stephen Walker <stephen.d.a.walker@gmail.com>
2022-09-05 23:18:01 -0500

    Update bash driver so a missing argument fails validation with str type

--- lib/drivers/bash_driver.sh
+++ lib/drivers/bash_driver.sh
@@ -80,7 +80,7 @@ bash_validate() {
   # Define available types
   declare -A verif
   local avail_types='str|int|bool|nowhite|upper|lower|single|ip'
-  local verif[str]='^.*$'
+  local verif[str]='^[^1].*$'
   local verif[int]='^[0-9]+$'
   local verif[bool]='^1$'
   local verif[nowhite]='^[^[:space:]]+$'
