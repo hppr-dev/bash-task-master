f0700f84f8f5f1ba89c19a6f55211ce446b253c4
Stephen Walker <stephen.d.a.walker@gmail.com>
2022-09-07 23:47:20 -0500

    Fix shell check for load-builtins.sh, state.sh and load-global. Make task-runner not executable.

--- lib/load-builtins.sh
+++ lib/load-builtins.sh
@@ -1,10 +1,10 @@
 # List of built in tasks
 # Comment out a task to disable it
-source $TASK_MASTER_HOME/lib/builtins/help.sh
-source $TASK_MASTER_HOME/lib/builtins/list.sh
-source $TASK_MASTER_HOME/lib/builtins/init.sh
-source $TASK_MASTER_HOME/lib/builtins/bookmark.sh
-source $TASK_MASTER_HOME/lib/builtins/goto.sh
-source $TASK_MASTER_HOME/lib/builtins/global.sh
-source $TASK_MASTER_HOME/lib/builtins/edit.sh
-source $TASK_MASTER_HOME/lib/builtins/module.sh
+source "$TASK_MASTER_HOME/lib/builtins/help.sh"
+source "$TASK_MASTER_HOME/lib/builtins/list.sh"
+source "$TASK_MASTER_HOME/lib/builtins/init.sh"
+source "$TASK_MASTER_HOME/lib/builtins/bookmark.sh"
+source "$TASK_MASTER_HOME/lib/builtins/goto.sh"
+source "$TASK_MASTER_HOME/lib/builtins/global.sh"
+source "$TASK_MASTER_HOME/lib/builtins/edit.sh"
+source "$TASK_MASTER_HOME/lib/builtins/module.sh"
